9e13c9c56a6227c2be836597dd9f637746ff15cf
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-09-10 19:25:33 +0530

    fix(effect): heterogenous types gone brr

--- src/hooks/effect/mod.rs
+++ src/hooks/effect/mod.rs
@@ -1,19 +1,16 @@
-use gpui::{Context, Entity, Subscription};
-use std::{cell::RefCell, rc::Rc};
+#[macro_export]
+macro_rules! create_effect {
+    ($cx:expr, [$($dep:expr),* $(,)?], $effect:expr) => {{
+        let effect = std::rc::Rc::new(std::cell::RefCell::new($effect));
+        let mut subs = Vec::new();
 
-pub fn create_effect<T: 'static, V: 'static>(
-    cx: &mut Context<V>,
-    deps: &[&Entity<T>],
-    mut effect: impl FnMut(&mut Context<V>) + 'static,
-) -> Vec<Subscription> {
-    let effect = Rc::new(RefCell::new(effect));
+        $(
+            let effect_clone = effect.clone();
+            subs.push($cx.observe(&$dep, move |_view, _entity, cx| {
+                (effect_clone.borrow_mut())(cx);
+            }));
+        )*
 
-    deps.iter()
-        .map(|dep| {
-            let effect = effect.clone();
-            cx.observe(dep, move |_view, _entity, cx| {
-                (effect.borrow_mut())(cx);
-            })
-        })
-        .collect()
+        subs
+    }};
 }
