dc374819379702e82440ba5b6613c66b57802a5e
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-09-10 18:55:42 +0530

    add: rewriter for cx

--- zopra_macros/src/lib.rs
+++ zopra_macros/src/lib.rs
@@ -1,6 +1,70 @@
 use proc_macro::TokenStream;
 use quote::ToTokens;
+use std::collections::HashSet;
+use syn::{
+    ExprCall, ExprClosure, ExprPath, Local, LocalInit, Pat, PatType,
+    visit_mut::{self, VisitMut},
+};
 use syn::{ItemFn, parse_macro_input, parse_quote};
+
+#[derive(Default)]
+struct InjectCx {
+    signal_idents: HashSet<syn::Ident>,
+    shadow_depth: usize,
+}
+
+// TODO: add more hooks to skip
+static SKIP_FNS: &[&str] = &["create_signal"];
+
+impl VisitMut for InjectCx {
+    fn visit_local_mut(&mut self, local: &mut Local) {
+        visit_mut::visit_local_mut(self, local);
+
+        if let Some(LocalInit { expr, .. }) = &local.init
+          && let syn::Expr::Call(ExprCall { func, .. }) = &**expr
+          && let syn::Expr::Path(ExprPath { path, .. }) = &**func
+          // only last cuz people use smtg::create_signal
+          && let Some(last_seg) = path.segments.last()
+          && SKIP_FNS.iter().any(|f| last_seg.ident == f)
+          && let Pat::Tuple(tuple) = &local.pat
+        {
+            self.signal_idents
+                .extend(tuple.elems.iter().filter_map(|elem| match elem {
+                    Pat::Ident(id) => Some(id.ident.clone()),
+                    _ => None,
+                }));
+        }
+    }
+
+    fn visit_expr_closure_mut(&mut self, closure: &mut ExprClosure) {
+        let shadows_cx = closure.inputs.iter().any(|input| match input {
+            Pat::Ident(p) => p.ident == "cx",
+            Pat::Type(PatType { pat, .. }) => {
+                matches!(pat.as_ref(), Pat::Ident(p) if p.ident == "cx")
+            }
+            _ => false,
+        });
+
+        if shadows_cx {
+            self.shadow_depth += 1;
+        }
+        visit_mut::visit_expr_closure_mut(self, closure);
+        if shadows_cx {
+            self.shadow_depth -= 1;
+        }
+    }
+
+    fn visit_expr_call_mut(&mut self, call: &mut ExprCall) {
+        visit_mut::visit_expr_call_mut(self, call);
+
+        if let syn::Expr::Path(ExprPath { path, .. }) = &*call.func
+            && let Some(seg) = path.segments.last()
+            && (SKIP_FNS.iter().any(|f| seg.ident == f) || self.signal_idents.contains(&seg.ident))
+        {
+            call.args.push(syn::parse_quote!(cx));
+        }
+    }
+}
 
 /// Converts a function into a GPUI component
 ///
@@ -16,6 +80,10 @@ pub fn component(_: TokenStream, item: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(item as ItemFn);
     input.sig.output = parse_quote!(-> impl gpui::IntoElement);
     input.sig.inputs.push(parse_quote!(cx: &mut gpui::App));
+
+    // injects cx into functions which need it
+    let mut rewriter = InjectCx::default();
+    rewriter.visit_block_mut(&mut input.block);
 
     TokenStream::from(input.to_token_stream())
 }
