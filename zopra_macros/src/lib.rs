5f002ba35de3afb9d438914444115d02cd14004d
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-09-10 18:26:39 +0530

    add: perfect zopra macro

--- zopra_macros/src/lib.rs
+++ zopra_macros/src/lib.rs
@@ -1,69 +1,21 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{Error, FnArg, ItemFn, Pat, parse_macro_input};
+use quote::ToTokens;
+use syn::{ItemFn, parse_macro_input, parse_quote};
 
-// TODO: later on if depending on signals are present or not, impl RenderOnce else Render like we do now
+/// Converts a function into a GPUI component
+///
+/// # Example
+/// ```rust
+/// #[component]
+/// fn button(text: 'static str) {
+///
+/// }
+/// ```
 #[proc_macro_attribute]
-pub fn component(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let function = parse_macro_input!(item as ItemFn);
+pub fn component(_: TokenStream, item: TokenStream) -> TokenStream {
+    let mut input = parse_macro_input!(item as ItemFn);
+    input.sig.output = parse_quote!(-> impl gpui::IntoElement);
+    input.sig.inputs.push(parse_quote!(cx: &mut gpui::App));
 
-    let name = &function.sig.ident;
-    let body = &function.block;
-
-    let mut fields = Vec::new();
-    let mut params = Vec::new();
-    let mut values = Vec::new();
-    let mut locals = Vec::new();
-
-    for arg in &function.sig.inputs {
-        let FnArg::Typed(arg) = arg else {
-            return Error::new_spanned(arg, "component cannot have self")
-                .to_compile_error()
-                .into();
-        };
-
-        let Pat::Ident(pattern) = &*arg.pat else {
-            return Error::new_spanned(&arg.pat, "component parameters must be identifiers")
-                .to_compile_error()
-                .into();
-        };
-
-        let name = &pattern.ident;
-        let ty = &arg.ty;
-
-        fields.push(quote! { #name: #ty });
-        params.push(quote! { #name: #ty });
-        values.push(quote! { #name });
-        locals.push(quote! { let #name = &self.#name; });
-    }
-
-    let constructor = quote! {
-        impl #name {
-            pub fn new(#(#params),*) -> Self {
-                Self {
-                    #(#values),*
-                }
-            }
-        }
-    };
-
-    quote! {
-        struct #name {
-            #(#fields),*
-        }
-
-        #constructor
-
-        impl Render for #name {
-            fn render(
-                &mut self,
-                cx: &mut ViewContext<Self>,
-            ) -> impl IntoElement {
-                #(#locals)*
-
-                #body
-            }
-        }
-    }
-    .into()
+    TokenStream::from(input.to_token_stream())
 }
