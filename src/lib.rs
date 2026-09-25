39c180a1ed1f007a8041f181bde355ba04d46d69
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-09-25 18:23:04 +0530

    add: cn component

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod config;
 pub mod hooks;
+pub mod utils;
 
 pub use zopra_macros::component;
 pub use zopra_macros::signals;
+pub use gpui_rsx::rsx as view;
---
+++ src/utils.rs
@@ -0,0 +1,40 @@
+pub trait ClassInput {
+    fn into_class_string(self) -> Option<String>;
+}
+
+impl ClassInput for &str {
+    fn into_class_string(self) -> Option<String> {
+        if self.is_empty() { None } else { Some(self.to_string()) }
+    }
+}
+
+impl ClassInput for String {
+    fn into_class_string(self) -> Option<String> {
+        if self.is_empty() { None } else { Some(self) }
+    }
+}
+
+impl<T: ClassInput> ClassInput for Option<T> {
+    fn into_class_string(self) -> Option<String> {
+        self.and_then(|t| t.into_class_string())
+    }
+}
+
+#[macro_export]
+macro_rules! cn {
+    ($($expr:expr),* $(,)?) => {
+        {
+            use $crate::utils::ClassInput;
+            let mut __classes = String::new();
+            $(
+                if let Some(__c) = $expr.into_class_string() {
+                    if !__classes.is_empty() {
+                        __classes.push(' ');
+                    }
+                    __classes.push_str(&__c);
+                }
+            )*
+            __classes
+        }
+    }
+}
